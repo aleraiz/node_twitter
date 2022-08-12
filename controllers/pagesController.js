28b03ef77fdcfa64d807865b8b67f15759586d2e
aleraiz <alejandroraiz7@gmail.com>
2022-08-12 16:39:08 -0300

    Mejoras en home view

--- controllers/pagesController.js
+++ controllers/pagesController.js
@@ -2,12 +2,26 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const Tweet = require("../models/Tweet");
 const formatDistanceToNow = require("date-fns/formatDistanceToNow");
+const lodash = require("lodash");
 require("../config/passportConfig");
 
 async function home(req, res) {
-  const tweets = await Tweet.find().sort({ createdAt: "descending" }).populate("author");
   const user = await User.findById(req.user.id);
-  res.render("home", { tweets, user, formatDistanceToNow });
+  const tweets = [];
+  const userTweet = await Tweet.find({ author: user.id })
+    .sort({ createdAt: "descending" })
+    .populate("author");
+  tweets.push(...userTweet);
+  for (let i = 0; i < user.following.length; i++) {
+    let tweet = await Tweet.find({ author: user.following[i] })
+      .sort({ createdAt: "descending" })
+      .populate("author");
+    tweets.push(...tweet);
+  }
+
+  const recomendedUsers = lodash.sampleSize(await User.find({ _id: { $ne: user.id } }), 3);
+
+  res.render("home", { tweets, user, recomendedUsers, formatDistanceToNow });
 }
 async function welcome(req, res) {
   res.render("welcome");
